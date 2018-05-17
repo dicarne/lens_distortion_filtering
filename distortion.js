f78e6fc779a81b7942d1821b93fc4b30b62aa0be
dicarne <hzdingjiahe@outlook.com>
2018-05-17 19:03:26 +0800

    add k3

--- distortion.js
+++ distortion.js
@@ -96,14 +96,18 @@ function webGLStart_inside(igl) {
 // end add
 var fboid;
 
-var alphax = 0.0;
-var alphax2 = 0.0;
-var alphay = 0.0;
-var alphay2 = 0.0;
+var k1x = 0.0;
+var k1xa = 0.0;
+var k1y = 0.0;
+var k1ya = 0.0;
 var k2x = 0.0;
 var k2y = 0.0;
 var k2xa = 0.0;
 var k2ya = 0.0;
+var k3x;
+var k3y;
+var k3xa = 0.0;
+var k3ya = 0.0;
 
 var maximum_alpha = 0.25;
 //var shaderProgram;
@@ -209,6 +213,8 @@ function initShaders(igl) {
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram.alphay = igl.getUniformLocation(igl.shaderProgram, "alphay"));
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram.k2x = igl.getUniformLocation(igl.shaderProgram, "k2x"));
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram.k2y = igl.getUniformLocation(igl.shaderProgram, "k2y"));
+    ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram.k3x = igl.getUniformLocation(igl.shaderProgram, "k3x"));
+    ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram.k3y = igl.getUniformLocation(igl.shaderProgram, "k3y"));
     // -------------------------
 
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram2 = igl.createProgram());
@@ -243,6 +249,8 @@ function initShaders(igl) {
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram2.alphay = igl.getUniformLocation(igl.shaderProgram2, "alphay"));
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram2.k2x = igl.getUniformLocation(igl.shaderProgram2, "k2x"));
     ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram2.k2y = igl.getUniformLocation(igl.shaderProgram2, "k2y"));
+    ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram2.k3x = igl.getUniformLocation(igl.shaderProgram2, "k3x"));
+    ctx = WebGLDebugUtils.makeDebugContext(igl.shaderProgram2.k3y = igl.getUniformLocation(igl.shaderProgram2, "k3y"));
 
 }
 
@@ -377,20 +385,26 @@ function drawScene(gl) {
     ctx = WebGLDebugUtils.makeDebugContext(gl.bindBuffer(gl.ARRAY_BUFFER, gl.vboId));
     var a, b, c, d;
     if (gl.rendercount <= 2) {
-        a = alphax;
-        b = alphay;
+        a = k1x;
+        b = k1y;
         c = k2x;
         d = k2y;
+        e = k3x;
+        f = k3y;
     } else {
         a = 0;
         b = 0;
         c = 0;
         d = 0;
+        e = 0;
+        f = 0;
     }
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram.alphax, new Float32Array([a])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram.alphay, new Float32Array([b])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram.k2x, new Float32Array([c])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram.k2y, new Float32Array([d])));
+    ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram.k3x, new Float32Array([e])));
+    ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram.k3y, new Float32Array([f])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.bindFramebuffer(gl.FRAMEBUFFER, gl.FBOs[0]));
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram.inPosition, 4, gl.FLOAT, false, 10 * 4, 0));
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram.inColor, 4, gl.FLOAT, false, 10 * 4, 4 * 4));
@@ -419,20 +433,26 @@ function drawScene(gl) {
     // Bind vertex data and set vertex attributes (as from a VAO)
     //ctx = WebGLDebugUtils.makeDebugContext(gl.bindBuffer(gl.ARRAY_BUFFER, gl.vboId));
     if (gl.rendercount == 2 || gl.rendercount == 3) {
-        a = alphax2;
-        b = alphay2;
+        a = k1xa;
+        b = k1ya;
         c = k2xa;
         d = k2ya;
+        e = k3xa;
+        f = k3ya;
     } else if (gl.rendercount == 1 || gl.rendercount == 4) {
         a = 0;
         b = 0;
         c = 0;
         d = 0;
+        e = 0;
+        f = 0;
     }
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram2.alphax, new Float32Array([a])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram2.alphay, new Float32Array([b])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram2.k2x, new Float32Array([c])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram2.k2y, new Float32Array([d])));
+    ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram2.k3x, new Float32Array([e])));
+    ctx = WebGLDebugUtils.makeDebugContext(gl.uniform1fv(gl.shaderProgram2.k3y, new Float32Array([f])));
     ctx = WebGLDebugUtils.makeDebugContext(gl.bindFramebuffer(gl.FRAMEBUFFER, gl.FBOs[1]));
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram2.inPosition, 4, gl.FLOAT, false, 10 * 4, 0));
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram2.inColor, 4, gl.FLOAT, false, 10 * 4, 4 * 4));
@@ -467,7 +487,7 @@ function drawJudge(gl) {
     ctx = WebGLDebugUtils.makeDebugContext(gl.bindBuffer(gl.ARRAY_BUFFER, gl.vboId));
     ctx = WebGLDebugUtils.makeDebugContext(gl.useProgram(gl.shaderProgram));
 
-
+    
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram.inPosition, 4, gl.FLOAT, false, 10 * 4, 0));
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram.inColor, 4, gl.FLOAT, false, 10 * 4, 4 * 4));
     ctx = WebGLDebugUtils.makeDebugContext(gl.vertexAttribPointer(gl.shaderProgram.inTextureCoord, 2, gl.FLOAT, false, 10 * 4, 8 * 4));
@@ -495,7 +515,7 @@ function initCompareTexture(igl) {
     texture = igl.createTexture();
     igl.compareTexture = texture;
 }
-
+var init = true;
 function createTexture(igl, array) {
 
     igl.activeTexture(igl.TEXTURE0);
@@ -505,10 +525,13 @@ function createTexture(igl, array) {
         igl.TEXTURE_2D, 0, igl.RGBA, 256, 256, 0, igl.RGBA,
         igl.UNSIGNED_BYTE, new Uint8Array(array)
     ));
-    ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_WRAP_S, igl.CLAMP_TO_EDGE));
-    ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_WRAP_T, igl.CLAMP_TO_EDGE));
-    ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_MIN_FILTER, igl.LINEAR));
-    ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_MAG_FILTER, igl.LINEAR));
+    if (init) {
+        ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_WRAP_S, igl.CLAMP_TO_EDGE));
+        ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_WRAP_T, igl.CLAMP_TO_EDGE));
+        ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_MIN_FILTER, igl.LINEAR));
+        ctx = WebGLDebugUtils.makeDebugContext(igl.texParameteri(igl.TEXTURE_2D, igl.TEXTURE_MAG_FILTER, igl.LINEAR));
+        init = false;
+    }
 }
 
 function paintLoop() {
@@ -520,14 +543,17 @@ function paintLoop() {
         drawScene(gl3);
         Judge();
     } else {
-        if (trainCount >= 50) {
-            scale = 20;
-        }
-        train();
-        if (trainCount >= 100) {
+        if (trainCount <= 50) {
+            train1();
+        } else if (trainCount <= 100) {
+            train2();
+        } else if (trainCount <= 150) {
+            train3();
+        } else {
             console.log('study end');
             showing = true;
             isFirstTime = true;
+            trainCount = 0;
         }
     }
 }
@@ -594,7 +620,7 @@ function cut_Predict() {
     return sum;
 }
 function cut() {
-    console.log('cut method');
+    //console.log('cut method');
     var maxpixcels = compare.width * compare.height * 4;
     var sum = 0;
     for (var x = 0; x < compare.width; x++) {
@@ -623,21 +649,27 @@ var same_alpha_factors = true; // Whether alphax and alphay are forced to be equ
 function adjustAlphaFactor(direction, value) {
     switch (direction) {
         case 'k1': {
-            alphax = (value * 0.01) * maximum_alpha;
-            alphay = alphax;
-            document.getElementById("k1_value").value = alphax;
+            k1x = (value * 0.01) * maximum_alpha;
+            k1y = k1x;
+            document.getElementById("k1_value").value = k1x;
             document.getElementById("k1_slider").value = value;
         } break;
         case 'k2': {
             k2x = (value * 0.01) * maximum_alpha;
             k2y = k2x;
             document.getElementById("k2_value").value = k2x;
-            document.getElementById("k2_slider2").value = value;
+            document.getElementById("k2_slider").value = value;
+        } break;
+        case 'k3': {
+            k3x = (value * 0.01) * maximum_alpha;
+            k3y = k3x;
+            document.getElementById("k3_value").value = k3x;
+            document.getElementById("k3_slider").value = value;
         } break;
         case 'k1a': {
-            alphax2 = (value * 0.01) * maximum_alpha;
-            alphay2 = alphax2;
-            document.getElementById("k1a_value2").value = alphax2;
+            k1xa = (value * 0.01) * maximum_alpha;
+            k1ya = k1xa;
+            document.getElementById("k1a_value2").value = k1xa;
             document.getElementById("k1a_slider2").value = value;
         } break;
         case 'k2a': {
@@ -646,32 +678,46 @@ function adjustAlphaFactor(direction, value) {
             document.getElementById("k2a_value2").value = k2xa;
             document.getElementById("k2a_slider2").value = value;
         } break;
+        case 'k3a': {
+            k3xa = (value * 0.01) * maximum_alpha;
+            k3ya = k3xa;
+            document.getElementById("k3a_value2").value = k3xa;
+            document.getElementById("k3a_slider2").value = value;
+        } break;
     }
 }
 
 function reset() {
-    alphax = 0.0;
-    alphax2 = 0.0;
-    alphay = 0.0;
-    alphay2 = 0.0;
+    k1x = 0.0;
+    k1xa = 0.0;
+    k1y = 0.0;
+    k1ya = 0.0;
     k2x = 0.0;
     k2y = 0.0;
     k2xa = 0.0;
     k2ya = 0.0;
-    document.getElementById("k1_value").value = alphax;
+    k3x = 0.0;
+    k3y = 0.0;
+    k3xa = 0.0;
+    k3ya = 0.0;
+    document.getElementById("k1_value").value = k1x;
     document.getElementById("k2_value").value = k2x;
-    document.getElementById("k1a_value2").value = alphax2;
+    document.getElementById("k3_value").value = k3x;
+    document.getElementById("k1a_value2").value = k1xa;
     document.getElementById("k2a_value2").value = k2xa;
+    document.getElementById("k3a_value2").value = k3xa;
 
-    document.getElementById("k1_slider").value = alphax;
+    document.getElementById("k1_slider").value = k1x;
     document.getElementById("k2_slider").value = k2x;
-    document.getElementById("k1a_slider2").value = alphax2;
+    document.getElementById("k3_slider").value = k3x;
+    document.getElementById("k1a_slider2").value = k1xa;
     document.getElementById("k2a_slider2").value = k2xa;
+    document.getElementById("k3a_slider2").value = k3xa;
 }
 // ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-
 
 function debug() {
-    train();
+    trainAll();
 
 }
 var judgemode = {
@@ -690,6 +736,7 @@ function changemode(str) {
     }
 }
 
+
 var isFirstTime = true;
 var lastValue = {};
 var loss = 0;
@@ -697,11 +744,11 @@ var trainCount = 0;
 function study() {
     console.log('start study!');
     showing = false;
-
 }
 var scale = 50;
-function train() {
-    console.log('trainning');
+
+function trainAll() {
+    //console.log('trainning');
     if (isFirstTime) {
         loss = red_predict();;
         trainCount = 0;
@@ -709,8 +756,9 @@ function train() {
         scale = 50;
     }
     trainCount++;
-    lastValue.k1 = alphax2;
+    lastValue.k1 = k1xa;
     lastValue.k2 = k2xa;
+    lastValue.k3 = k3xa;
     function normal(value) {
         return (value * 0.01) * maximum_alpha
     }
@@ -720,28 +768,124 @@ function train() {
 
     var ch1 = (Math.random() - 0.5) * 50 + denormal(lastValue.k1);
     var ch2 = (Math.random() - 0.5) * 50 + denormal(lastValue.k2);
+    var ch3 = (Math.random() - 0.5) * 50 + denormal(lastValue.k3);
+
     adjustAlphaFactor('k1a', ch1);
     adjustAlphaFactor('k2a', ch2);
+    adjustAlphaFactor('k3a', ch3);
+
     drawScene(gl2);
     //drawScene(gl3);
     var loss2 = red_predict();
-    console.log(loss2);
+    //console.log(loss2);
     if (loss2 < loss) {
-        console.log('success');
+        //console.log('success');
         Judge();
-        lastValue.k1 = alphax2;
+        lastValue.k1 = k1xa;
+        lastValue.k2 = k2xa;
+        lastValue.k3 = k3xa;
+        loss = loss2;
+    } else {
+        //console.log('roll back');
+        k1xa = lastValue.k1;
+        k2xa = lastValue.k2;
+        k3xa = lastValue.k3;
+    }
+    adjustAlphaFactor('k1a', denormal(k1xa));
+    adjustAlphaFactor('k2a', denormal(k2xa));
+    adjustAlphaFactor('k3a', denormal(k3xa));
+
+}
+
+function train1() {
+    //console.log('trainning');
+    if (isFirstTime) {
+        console.log('train start');
+        loss = red_predict();;
+        trainCount = 0;
+        isFirstTime = false;
+        showing = false;
+        scale = 50;
+    }
+    trainCount++;
+    lastValue.k1 = k1xa;
+
+    var ch1 = (Math.random() - 0.5) * 50 + denormal(lastValue.k1);
+
+    adjustAlphaFactor('k1a', ch1);
+
+    drawScene(gl2);
+    //drawScene(gl3);
+    var loss2 = red_predict();
+    //console.log(loss2);
+    if (loss2 < loss) {
+        //console.log('success');
+        Judge();
+        lastValue.k1 = k1xa;
+        loss = loss2;
+    } else {
+        //console.log('roll back');
+        k1xa = lastValue.k1;
+    }
+    adjustAlphaFactor('k1a', denormal(k1xa));
+
+}
+
+function train2() {
+
+    trainCount++;
+    lastValue.k2 = k2xa;
+
+    var ch2 = (Math.random() - 0.5) * 50 + denormal(lastValue.k2);
+
+    adjustAlphaFactor('k2a', ch2);
+
+    drawScene(gl2);
+    //drawScene(gl3);
+    var loss2 = red_predict();
+    //console.log(loss2);
+    if (loss2 < loss) {
+        //console.log('success');
+        Judge();
         lastValue.k2 = k2xa;
         loss = loss2;
-        //adjustAlphaFactor('k1a', alphax2);
-        //adjustAlphaFactor('k2a', k2xa);
     } else {
-        console.log('roll back');
-        alphax2 = lastValue.k1;
+        //console.log('roll back');
         k2xa = lastValue.k2;
-        //adjustAlphaFactor('k1a', alphax2);
-        //adjustAlphaFactor('k2a', k2xa);
     }
-    adjustAlphaFactor('k1a', denormal(alphax2));
     adjustAlphaFactor('k2a', denormal(k2xa));
 
+}
+
+function train3() {
+
+    trainCount++;
+    lastValue.k3 = k3xa;
+
+    var ch3 = (Math.random() - 0.5) * 50 + denormal(lastValue.k3);
+
+    adjustAlphaFactor('k3a', ch3);
+
+    drawScene(gl2);
+    //drawScene(gl3);
+    var loss2 = red_predict();
+    //console.log(loss2);
+    if (loss2 < loss) {
+        //console.log('success');
+        Judge();
+        lastValue.k3 = k3xa;
+        loss = loss2;
+    } else {
+        //console.log('roll back');
+        k3xa = lastValue.k3;
+    }
+    adjustAlphaFactor('k3a', denormal(k3xa));
+
+}
+
+function normal(value) {
+    return (value * 0.01) * maximum_alpha
+}
+function denormal(value) {
+    return value / 0.01 / maximum_alpha;
 }
